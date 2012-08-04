15ab46fc249c9d05fb3ed414679cc2c564b8d67d
Elliot Wright <wright.elliot@gmail.com>
2012-08-04 15:12:33 +0100

    Setting up plugin folder creation.

--- src/net/unknown_degree/seer/Professions/Professions.java
+++ src/net/unknown_degree/seer/Professions/Professions.java
@@ -5,15 +5,27 @@ import java.io.File;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Professions extends JavaPlugin {
-	public void onEnable(){ 
-		 getLogger().info("Settler enabled");
+	public void onEnable(){
+		
+		 getLogger().info("Professions enabled");
 		 //getServer().getPluginManager().registerEvents(new PlayerListener(), this);
 		 
-		 //Boolean test = (new File("./plugins/settler/").exists());
-		 //String test2 = test.toString();
-		 //getLogger().info(test2);
+		 /*
+		  * Set up plug-in folders:
+		  */
+		 File f = new File("./plugins/professions/");
+		 if ( !f.exists() ) {
+			 getLogger().info("Professions directory not found. Attempting creation...");
+			 try {
+				 f.mkdir();
+				 getLogger().info("....\nProfessions directory created successfully!");
+			 } catch (Exception e) {
+				 getLogger().info("....\nProfessions directory was NOT created!");
+			 }
+		 }
+		 
 	}
 	public void onDisable(){ 
-		 getLogger().info("Settler disabled.");
+		 getLogger().info("Professions disabled.");
 	}
 }
